76464d78db3ce4ea814d1eb88397395093bc626d
Aptitudeland <jb.garnier@gmail.com>
2024-05-23 12:56:34 +0200

    Add comments for  test
    Adding comments for git desktop test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,5 @@ class Movie < ApplicationRecord
   has_many :lists, through: :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+  # comment for test
 end
