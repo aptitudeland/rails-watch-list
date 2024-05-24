77609a16b5eba3f46cee2855c0191dd94fd6b0e9
Aptitudeland <jb.garnier@gmail.com>
2024-05-24 12:14:07 +0200

    rails-watch-list-update

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    redirect_to @bookmark, notice: 'Bookmark was successfully created.'
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,31 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie, notice: 'Movie was successfully created.'
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, status: :see_other
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :rating, :poster_url)
+  end
+end
